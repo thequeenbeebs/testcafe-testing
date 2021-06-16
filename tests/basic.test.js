2264c6f0450024b9feec8d76eab67d52cdddeff7
Blaire Baker <bakerblaire@gmail.com>
2021-06-16 17:52:31 -0600

    write first basic test

---
+++ tests/basic.test.js
@@ -0,0 +1,9 @@
+import { Selector } from 'testcafe'
+
+fixture `Getting started with testCafe`
+    .page `https://devexpress.github.io/testcafe/example/`
+
+    test('My first testcafe test', async t => {
+        await t.typeText("#developer-name", "John")
+        await t.click("#submit-button")
+    })
